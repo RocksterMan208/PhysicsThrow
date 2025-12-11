52d2146a0fe0fb7eca891a2d280b001741822eb4
RocksterMan208 <troniclife12@gmail.com>
2025-12-11 12:37:24 +0000

    Added click dragging to the cube.

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,47 @@ int main()
 {
     float screenWidth = 800;
     float screenHeight = 600;
+    float size = 40;
+
 
     InitWindow(screenWidth, screenHeight, "ThrowPhysics");
     SetTargetFPS(60);
 
-    Mouse mouse;
+    Rectangle object(0,0,size,size);
 
+    Vector2 dragOffset;
+    bool interacting = false;
+    
     while (!WindowShouldClose())
     {
+        Vector2 mousePos = GetMousePosition();
+
+
+        
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 mousePos = mouse.getpos();
-        std::cout << "Mouse:" << mousePos.x << " | "<< mousePos.y << std::endl;
 
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            if (CheckCollisionPointRec(mousePos, object))
+            {
+                interacting = true;
+                dragOffset.x = mousePos.x - object.x;
+                dragOffset.y = mousePos.y - object.y;
+            }
+        }
+
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) interacting = false;
+
+        if (interacting)
+        {
+            object.x = mousePos.x - dragOffset.x;
+            object.y = mousePos.y - dragOffset.y;
+        }
+
+        DrawRectangleRec(object,RED);
+        
         EndDrawing();
     }
     return 0;
